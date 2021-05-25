99184576f0e5f8831da6004d489264d66dceeb70
AndreCorreaSantos <acorreasantos2002@gmail.com>
2021-05-25 16:19:14 -0300

    Começando a implementar um menu.

--- Main.py
+++ Main.py
@@ -35,90 +35,110 @@ for i in range(50):
     invasor.spawn(mapa)
 
 conta_kills = 0
+click = False
+start = False
 
 while running: 
     #inicializando clock que cuida do fps
     background.fill((0,0,0))
     screen.blit(background,(0,0))
+    #construindo menu
+    button_width = 500
+    button_height = 100
+    mx,my = pygame.mouse.get_pos()
+    button1 = pygame.Rect(width/2-button_width/2,height/2-button_height/2-100,button_width,button_height)
+    button2 = pygame.Rect(width/2-button_width/2,height/2-button_height/2+100,button_width,button_height)
+    if button1.collidepoint((mx,my)):
+        if click:
+            start = True
+    if button2.collidepoint((mx,my)):
+        if click:
+            pass
+    pygame.draw.rect(screen,(255,0,0),button1)
+    pygame.draw.rect(screen,(255,0,0),button2)
+
     #checando quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                click = True
+    if start == True:
+        #checando eventos criando tiros
+        m1 = pygame.mouse.get_pressed()[0]
+        m2 = pygame.mouse.get_pressed()[2]
+        m3 = pygame.mouse.get_pressed()[1]
+        if m1:
+            now_w = pygame.time.get_ticks()
+            if now_w - start_w >= 100:
+                bala = tiro(p1)
+                bala.rotate()
+                tiros.add(bala)
+                all_sprites.add(bala)
+                start_w = now_w
+        if m2:
+            now_t = pygame.time.get_ticks()
+            if now_t - start_t >= 100:
+                bala2 = tiro(p1)
+                bala2.rotate()
+                tiros.add(bala2)
+                all_sprites.add(bala2)
+                start_t = now_t
+        #updatando invasore e tiros
+        tiros.update()
+        for i in invasores:
+            i.update(mapa)
+            #print(i.rect.x)
+        #print(len(invasores))
+        #checando colisoes, aplicando colisoes e respawnando invasores
+        col = pygame.sprite.groupcollide(tiros,invasores,True,True)
+        for i in col:
+            x_aleatorio = mapa.rect.x+400
+            y_aleatorio = mapa.rect.y+400
+            i = invasor(x_aleatorio,y_aleatorio,random.randint(-5,5),random.randint(-5,5))
+            all_sprites.add(i)
+            invasores.add(i)
+            conta_kills += 1
+        #recebendo e aplicando os parametros para fazer o offset da camera
+        velocidade = 6
+        vx,vy = camera_update(velocidade)
+        #booleanas para saber se o mapa atingiu limite x ou y do mapa e entao parar de aplicar o offset nas outras entidades.
+        testex,testey = mapa.offset()
+        mapa.rect.x += vx
+        mapa.rect.y += vy
+        mapa.draw(screen)   
+        for i in all_sprites:
+            if type(i) == player:
+                v1x = 0
+                v1y = 0
+                i.rotate()
+                i.draw(screen)
+            elif type(i) == invasor:
+                if testex:
+                    i.rect.x += vx
+                if testey:
+                    i.rect.y += vy
+                i.move_towards_player(p1)
+                i.draw(screen)
+            else:
+                if testex:
+                    i.rect.x += vx
+                if testey:
+                    i.rect.y += vy
+                i.draw(screen)
 
-    #checando eventos criando tiros
-    m1 = pygame.mouse.get_pressed()[0]
-    m2 = pygame.mouse.get_pressed()[2]
-    m3 = pygame.mouse.get_pressed()[1]
-    if m1:
-        now_w = pygame.time.get_ticks()
-        if now_w - start_w >= 100:
-            bala = tiro(p1)
-            bala.rotate()
-            tiros.add(bala)
-            all_sprites.add(bala)
-            start_w = now_w
-    if m2:
-        now_t = pygame.time.get_ticks()
-        if now_t - start_t >= 100:
-            bala2 = tiro(p1)
-            bala2.rotate()
-            tiros.add(bala2)
-            all_sprites.add(bala2)
-            start_t = now_t
-    #updatando invasore e tiros
-    tiros.update()
-    for i in invasores:
-        i.update(mapa)
-        #print(i.rect.x)
-    #print(len(invasores))
-    #checando colisoes, aplicando colisoes e respawnando invasores
-    col = pygame.sprite.groupcollide(tiros,invasores,True,True)
-    for i in col:
-        x_aleatorio = mapa.rect.x+400
-        y_aleatorio = mapa.rect.y+400
-        i = invasor(x_aleatorio,y_aleatorio,random.randint(-5,5),random.randint(-5,5))
-        all_sprites.add(i)
-        invasores.add(i)
-        conta_kills += 1
-    #recebendo e aplicando os parametros para fazer o offset da camera
-    velocidade = 6
-    vx,vy = camera_update(velocidade)
-    #booleanas para saber se o mapa atingiu limite x ou y do mapa e entao parar de aplicar o offset nas outras entidades.
-    testex,testey = mapa.offset()
-    mapa.rect.x += vx
-    mapa.rect.y += vy
-    mapa.draw(screen)
-    for i in all_sprites:
-        if type(i) == player:
-            v1x = 0
-            v1y = 0
-            i.rotate()
-            i.draw(screen)
-        elif type(i) == invasor:
-            if testex:
-                i.rect.x += vx
-            if testey:
-                i.rect.y += vy
-            i.move_towards_player(p1)
-            i.draw(screen)
-        else:
-            if testex:
-                i.rect.x += vx
-            if testey:
-                i.rect.y += vy
-            i.draw(screen)
+        if conta_kills > 50:
+            mapa = mapa2
+        if conta_kills > 100:
+            mapa = mapa3
 
-    if conta_kills > 50:
-        mapa = mapa2
-    if conta_kills > 100:
-        mapa = mapa3
-    
-    pygame.font.init()
-    myfont = pygame.font.SysFont("Arial",50)
-    textsurface = myfont.render("Kills: {}".format(conta_kills),False,(0,0,0))
-    screen.blit(textsurface,(0,0))
-    
-    #funcao para desenhar a hitbox dos sprites, debug
-    draw_hb(all_sprites,screen)
-    p1.draw_hitbox(screen)
+        pygame.font.init()
+        myfont = pygame.font.SysFont("Arial",50)
+        textsurface = myfont.render("Kills: {}".format(conta_kills),False,(0,0,0))
+        screen.blit(textsurface,(0,0))
+
+        #funcao para desenhar a hitbox dos sprites, debug
+        draw_hb(all_sprites,screen)
+        p1.draw_hitbox(screen)
     pygame.display.update()
